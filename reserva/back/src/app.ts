b3868cf97a72c6d78430716910f72b8767ea2fc0
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-04-17 16:00:44 -0300

    delete stuff

--- reserva/back/src/app.ts
+++ reserva/back/src/app.ts
@@ -5,12 +5,8 @@ import { errorHandler } from "./middleware/errorHandler"
 import {PatientRepository} from './core/components/patient/repository'
 import {getRepository, MainDataSource} from './config/db/data-source'
 import {Patient} from './core/components/patient/entity'
-import { exameRouter } from "./core/components/exame/router";
-import { acompanhamentoRouter } from "./core/components/acompanhamento/router";
-import { consultaRouter } from "./core/components/consulta/router";
-import { documentoRouter } from "./core/components/documento/router";
 import { patientRouter } from "./core/components/patient/router";
-import { relacionamentoRouter } from "./core/components/relacionamentos/router";
+
 
 
 const PORT = env.serverPort
@@ -34,33 +30,12 @@ async function main() {
 
 	app.use(errorHandler);
 
-	app.use("/acompanhamento", acompanhamentoRouter);
-	app.use("/consulta", consultaRouter);
-	app.use("/documento", documentoRouter);
-	app.use("/exame", exameRouter);
 	app.use("/patient", patientRouter);
-	app.use("/relacionamento", relacionamentoRouter);
-
-
-	// app.get('/teste', async (req, res) => {
-	// 	const repository = new PatientRepository(getRepository(Patient))
-	// 	const patient = new Patient('teste1@on.com','senha','teste1')
-
-	// 	repository.create(patient)
-
-	// 	const entity = await repository.findOneByEmail('teste1@on.com')
-
-	// 	return res.status(200).json(entity)
-
-	// })
-
 
 	app.listen(PORT, () => {
 		console.log(`Server running on port ${PORT}`)
 	})
 }
-
-
 
 main().catch(error => {
 	log.error('catch main application')
