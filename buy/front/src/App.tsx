bc7b35fa774be20faf301da39820e91c5f1f3772
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-04-21 17:25:30 -0300

    test

--- buy/front/src/App.tsx
+++ buy/front/src/App.tsx
@@ -4,9 +4,10 @@ import { URL_PATHS } from './services/apiPaths';
 
 
 function App() {
-  const [emailPaciente, setEmailPaciente] = useState('');
+  const [email, setEmail] = useState('');
+  
 
-  const [dados, setDados] = useState<any | null >(null);
+  const [dados, setDados] = useState<any | null>(null);
   const [erroMessage, setErroMessage] = useState<string | null>(null);
 
   // console.log(user, process.env.NODE_ENV)
@@ -14,18 +15,9 @@ function App() {
   // const urlParams = new URLSearchParams(window.location.search);
   // const customToken = urlParams.get('token') ?? ''
 
-  // useEffect(() => {
-  //   if (customToken) {
-  //     signInWithCustomToken(auth, customToken).then((user) => {
-  //       console.log('aqui', user)
-  //       if (user) {
-  //         console.log("Usuário logado:", user);
-  //         setUser(user);
-  //       }
-  //     })
-  //   }
+  useEffect(() => {
 
-  // }, []);
+  }, []);
 
   //  if (!user && process.env.NODE_ENV === 'production') {
   //    return <div>Carregando...</div>;
@@ -44,22 +36,88 @@ function App() {
       setDados(response.data);
       setErroMessage(null);
     } catch (error) {
-      setErroMessage('Ocorreu um erro ao buscar os exames. Por favor, tente novamente.');
+      setErroMessage('Erro de usuario');
       setDados(null);
     }
   };
+
+  const handlePost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(e)
+    try {
+      const response = await Api.get(URL_PATHS.GET_USER, {
+        params: {
+          email: emailPaciente,
+          assento: 2
+        },
+      });
+
+      setDados(response.data);
+      setErroMessage(null);
+    } catch (error) {
+      setErroMessage('Erro de ingresso');
+      setDados(null);
+    }
+  };
+
+  const arrayCom25Elementos: number[] = Array.from({ length: 25 }, (_, index) => index + 1);
 
   return (
     <>
       test api call
       <form onSubmit={handleSubmit}>
-            <button
-              type="submit"
-              className="py-2 px-4 bg-indigo-600 text-white rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
-            >
-              Get simples
-            </button>
-          </form>
+        <button
+          type="submit"
+          className="py-2 px-4 bg-indigo-600 text-white rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+        >
+          Get simples
+        </button>
+      </form>
+      <div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email
+          </label>
+          <input
+            type="string"
+            id="email"
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            placeholder="Digite seu email"
+            value={email || ''}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Header 1</th>
+              <th>Header 2</th>
+              <th>Header 3</th>
+              <th>Header 4</th>
+              <th>Header 5</th>
+            </tr>
+          </thead>
+          <tbody>
+            {arrayCom25Elementos.map((item, index) => (
+              <tr key={index}>
+                <td>{
+                  <form onSubmit={handlePost()}>
+                    <button
+                      type="submit"
+                      className="py-2 px-4 bg-indigo-600 text-white rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                    >
+                      Get simples
+                    </button>
+                  </form>
+                }</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
--- buy/front/src/services/apiPaths.ts
+++ buy/front/src/services/apiPaths.ts
@@ -1,6 +1,7 @@
 export const URL_PATHS = {
-    GET_USER: 'usuario/get/all',
+    GET_USER: '/usuario/get/all',
     POST_USER: '/usuario/new',
+    POST_INGRESSO: '/ingresso/create',
     // VISUALIZAR_AGENDA: '/patient/vizualizar/agenda',
     // VISUALIZAR_EXAMES: '/patient/vizualizar/exame',
     // POSTAR_EXAMES: '/patient/postar/exame',
